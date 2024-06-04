887c7245ddbfe38952686aa808c4fdd3e1a7eb75
seveibar <seveibar@gmail.com>
2024-06-04 13:06:12 -0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import "./App.css"
-
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-2xl">Header</h1>
+      <header className="p-4">
+        <h1 className="text-md">kicad viewer</h1>
       </header>
       <div className="flex flex-1">
         <aside className="bg-gray-200 w-1/4 p-4">
-          <h2 className="text-xl">Sidebar</h2>
+          <h2 className="text-md">Sidebar</h2>
         </aside>
         <section className="flex-1 p-4">
-          <h2 className="text-xl">Content</h2>
+          <h2 className="text-md">Content</h2>
         </section>
       </div>
-      <footer className="bg-blue-500 text-white p-4">
-        <h2 className="text-xl">Footer</h2>
+      <footer className="p-4 text-xs text-center">
+        made with tscircuit & <a href="https://x.com/seveibar">@seveibar</a>
       </footer>
     </div>
   )
