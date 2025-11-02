c87ff7dcbadb22056841598ea894c36c5a6b5ccb
techmannih <manishchaudharyttt@gmail.com>
2025-11-02 10:49:04 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: {},
-  },
-  base: "https://tscircuit.github.io/kicad-viewer",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://kicad-mod-cache.tscircuit.com/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-})
+export default defineConfig(({ command }) => {
+	const isVercel = "1";
+	const base =
+		command === "serve" || isVercel
+			? "/"
+			: "https://tscircuit.github.io/kicad-viewer";
+
+	return {
+		plugins: [react()],
+		define: {
+			global: {},
+		},
+		base,
+		server: {
+			proxy: {
+				"/api": {
+					target: "https://kicad-mod-cache.tscircuit.com/",
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
+			},
+		},
+	};
+});
