b4e47fe904b883a7d22ba3ac43814117dc4a7316
techmannih <manishchaudharyttt@gmail.com>
2025-11-12 01:12:54 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "./index.css"
-import ContextProviders from "./ContextProviders"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import ContextProviders from "./ContextProviders";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ContextProviders>
-      <App />
-    </ContextProviders>
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<ContextProviders>
+			<App />
+		</ContextProviders>
+	</React.StrictMode>,
+);
